a81fc69dafa0c233705647cc587ad91bec286876
juniscottmatagalpa <juniscottmatagalpa@gmail.com>
2025-11-11 09:57:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const scrapeVideoUrl = require('./scraper');
+const { scrapeVideoUrl, downloadVideo } = require('./scraper');
 const app = express();
 const port = 3000;
 
@@ -10,6 +10,18 @@ app.post('/scrape', async (req, res) => {
   try {
     const videoUrl = await scrapeVideoUrl(url);
     res.json({ videoUrl });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/download', async (req, res) => {
+  const { videoUrl } = req.query;
+  try {
+    const data = await downloadVideo(videoUrl);
+    res.setHeader('Content-Type', 'video/mp4');
+    res.setHeader('Content-Disposition', 'attachment; filename=sora_video.mp4');
+    res.send(Buffer.from(data));
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
